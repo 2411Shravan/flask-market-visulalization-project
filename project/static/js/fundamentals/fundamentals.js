d6bce39204120767a3af1376f54867743fda55de
Shravan .V <perfectparameter24@gmail.com>
2021-08-11 19:02:46 +0530

    Added stock symbols with frontend pagination

--- project/static/js/fundamentals/fundamentals.js
+++ project/static/js/fundamentals/fundamentals.js
@@ -1,7 +1,10 @@
 const spinner_load = document.getElementById('load_spinner');
+const head = document.getElementById("head")
 
+const arr_stock=[];
 function getStock(){
     spinner_load.style.display = 'block';
+    head.style.display = 'none';
     console.log('hwllo world');
     var api = 'https://finnhub.io/api/v1/stock/symbol?exchange=US&token=c2vgio2ad3i9mrpv9i2g';
     getStocks(api);
@@ -10,6 +13,114 @@ function getStock(){
 async function getStocks(API){
     const response = await fetch(API);
     const responseData=await response.json();
-    console.log(responseData);
+   // console.log(responseData);
+    showStocks(responseData);
+    
+}
+function showStocks(datas){
+
+    datas.forEach(data=>{
+        var obj={};
+        obj['name']=data.description;
+        obj['currency']=data.currency;
+        obj['symbol']=data.displaySymbol;
+        // obj['mic']=data.mic;
+        obj['type']=data.type;
+      //  console.log(data);
+        arr_stock.push(obj);
+    });
+
     spinner_load.style.display = 'none';
+    console.log(arr_stock);
+    buildTable();
+}
+
+var state = {
+    'querySet': arr_stock,
+    'page': 1,
+    'rows': 25,
+    'window': 5,
+}
+
+function pagination(querySet, page, rows) {
+
+    var trimStart = (page - 1) * rows
+    var trimEnd = trimStart + rows
+
+    var trimmedData = querySet.slice(trimStart, trimEnd)
+
+    var pages = Math.round(querySet.length / rows);
+
+    return {
+        'querySet': trimmedData,
+        'pages': pages,
+    }
+}
+
+function pageButtons(pages) {
+    var wrapper = document.getElementById('pagination-wrapper')
+
+    wrapper.innerHTML = ``
+	console.log('Pages:', pages)
+
+    var maxLeft = (state.page - Math.floor(state.window / 2))
+    var maxRight = (state.page + Math.floor(state.window / 2))
+
+    if (maxLeft < 1) {
+        maxLeft = 1
+        maxRight = state.window
+    }
+
+    if (maxRight > pages) {
+        maxLeft = pages - (state.window - 1)
+        
+        if (maxLeft < 1){
+        	maxLeft = 1
+        }
+        maxRight = pages
+    }
+    
+    
+
+    for (var page = maxLeft; page <= maxRight; page++) {
+    	wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-info">${page}</button>`
+    }
+
+    if (state.page != 1) {
+        wrapper.innerHTML = `<button value=${1} class="page btn btn-sm btn-info">&#171; First</button>` + wrapper.innerHTML
+    }
+
+    if (state.page != pages) {
+        wrapper.innerHTML += `<button value=${pages} class="page btn btn-sm btn-info">Last &#187;</button>`
+    }
+
+    $('.page').on('click', function() {
+        $('#table-body').empty()
+
+        state.page = Number($(this).val())
+
+        buildTable()
+    })
+
+}
+
+function buildTable() {
+    var table = $('#table-body')
+
+    var data = pagination(state.querySet, state.page, state.rows)
+    var myList = data.querySet;
+    head.style.display = 'block';
+    for (var i = 1 in myList) {
+        //Keep in mind we are using "Template Litterals to create rows"
+        var row = `<tr class="change-font">
+                  <td >${myList[i].currency}</td>
+                  
+                  <td >${myList[i].name}</td>
+                  <td >${myList[i].symbol}</td>
+                  <td >${myList[i].type}</td>
+                  `
+        table.append(row)
+    }
+
+    pageButtons(data.pages)
 }
