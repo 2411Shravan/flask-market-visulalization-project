d447b4c63b68c55a14883ed45bc5898635a7e95e
Shravan .V <perfectparameter24@gmail.com>
2021-08-13 22:47:36 +0530

    added some api in fundamentals section

--- project/fundamentals.py
+++ project/fundamentals.py
@@ -5,6 +5,8 @@ from flask import redirect
 from flask import flash
 from flask_login import current_user
 from flask_login import login_required
+from flask import request
+from pprint import pprint
 import requests
 
 fundamentals = Blueprint('fundamentals',__name__)
@@ -20,7 +22,17 @@ def stock_symbols():
 def all_stocks():
     return render_template('fundamentals/allstocks.html',user=current_user)
 
-@fundamentals.route('/fundamentals/company-profile/')
+@fundamentals.route('/fundamentals/company-profile/',methods=['GET','POST'])
 @login_required
 def profile():
+
+    if request.method == 'POST':
+        comp_name = request.form['company']
+        url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol='+comp_name+'&apikey=WH75LQJ4BD7S15TO'
+        r = requests.get(url)
+        data = r.json()
+
+        # pprint(data)
+        return render_template('fundamentals/company_profile.html',user=current_user)
+        
     return render_template('fundamentals/company_profile.html',user=current_user)
